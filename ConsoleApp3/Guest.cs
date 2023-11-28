40252cd95f4225f68aef293e74af2565b6267219
Mike Chafin <support@websunlimited.com>
2023-11-28 09:14:51 -0700

    Cleaned up using

--- ConsoleApp3/Guest.cs
+++ ConsoleApp3/Guest.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ConsoleApp3;
 
-namespace ConsoleApp3
+public class Guest
 {
-    public class Guest
-    {
-        public string? partyName { get; set; }
-        public int partySize { get; set; }
-    }
+    public string? partyName { get; set; }
+    public int partySize { get; set; }
 }
